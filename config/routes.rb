322c540474a616c5af36a10b348c43a371f07033
Michael-Bradley Timm <michaelbtimm@gmail.com>
2023-05-23 16:26:56 +0200

    Added the Articles code

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # CREATE
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  post 'articles', to: 'articles#create'
+  # READ ALL
+  get 'articles', to: 'articles#index'
+  # READ ONE
+  get 'articles/:id', to: 'articles#show', as: :article
+  # UPDATE
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+  # DELETE
+  delete 'articles/:id', to: 'articles#destroy'
 end
